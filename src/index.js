3ce6f685eb1191fad3b0e7cad25ed07e07ff03ba
Ivan Ivanov <ivanov-i.s@mail.ru>
2021-02-28 17:40:13 +0300

    feat: implement toReadable function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function toReadable (number) {
-  
+    let a = ['','one','two','three','four', 'five','six','seven','eight','nine','ten',
+            'eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen'];
+    let b = ['', '', 'twenty','thirty','forty','fifty', 'sixty','seventy','eighty','ninety'];
+    let str = "" + number;
+    let twoDigitToWords = arg => arg[0] > 1 ? b[+(arg[0])] + " " + a[+(arg[1])] : a[+(arg[0] + arg[1])];
+    if (number === 0) return "zero"
+    else 
+    switch(str.length){
+        case 1 : return (a[+(str[0])]).trim(); 
+        case 2 : return (twoDigitToWords(str)).trim();
+        case 3 : return (a[str[0]] + " hundred " + twoDigitToWords(str[1] + str[2])).trim();
+    }
 }
